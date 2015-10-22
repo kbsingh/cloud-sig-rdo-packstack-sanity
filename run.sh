f3bf33dabe0c4a5e9832540cc77997171ca9d181
Karanbir Singh <kbsingh@karan.org>
2015-10-22 10:29:35 +0100

    use the demo user; fix type for keypair add

--- run.sh
+++ run.sh
@@ -15,12 +15,12 @@ if [ $? -ne 0 ]; then
 fi
 
 # Get the CentOS-7 qcow2, import into glance, setup security group, run an instance
-. ~/keystonerc_admin
+. ~/keystonerc_demo
 curl -O http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
 glance image-create --name='CentOS7' --container-format=bare --disk-format=qcow2 < CentOS-7-x86_64-GenericCloud.qcow2 
 nova secgroup-create gen_sg "generic :22 and icmp"
 nova secgroup-add-rule gen_sg icmp 0 255 0.0.0.0/0
 nova secgroup-add-rule gen_sg tcp 22 22 0.0.0.0/0
-nova keypaid-add --pub-key ~/.ssh/id_rsa.pub gen_kp
+nova keypair-add --pub-key ~/.ssh/id_rsa.pub gen_kp
 nova boot --image="CentOS7" --flavor="m1.small" --key_name gen_kp --security-groups gen_sg i1
 
