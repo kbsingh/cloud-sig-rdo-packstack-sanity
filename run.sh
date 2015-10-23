3efae727ee7f5d80df2f3dfd25597258dfa3467e
Karanbir Singh <kbsingh@karan.org>
2015-10-23 10:07:07 +0100

    get the t_functional tests going

--- run.sh
+++ run.sh
@@ -23,4 +23,12 @@ nova secgroup-add-rule gen_sg icmp 0 255 0.0.0.0/0
 nova secgroup-add-rule gen_sg tcp 22 22 0.0.0.0/0
 nova keypair-add --pub-key ~/.ssh/id_rsa.pub gen_kp
 nova boot --image="CentOS7" --flavor="m1.small" --key_name gen_kp --security-groups gen_sg i1
+sleep 5
+
+# get the netns setup and wait for ping to start working
+net_id=$(neutron router-list -F id -f value )
+IP=$(openstack server list -c Networks -f value --name i1 | cut -f 2 -d'=')
+ip netns exec qrouter-da282ee0-e590-4aee-bc89-d0c9d037b5b7 ssh-keyscan -t ecdsa $IP >> ~/.ssh/known_hosts
+ssh_cmd="ip netns exec qrouter-da282ee0-e590-4aee-bc89-d0c9d037b5b7 ssh"
+$ssh_cmd centos@${IP} "yum -y install git && git clone https://git.centos.org/git/sig-code/t_functional && cd t_functonal && git checkout lightweight && runtests.sh"
 
