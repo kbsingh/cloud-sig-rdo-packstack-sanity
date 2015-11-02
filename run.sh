6cde88e80b67cb79d12e591cd804117701751321
Karanbir Singh <kbsingh@karan.org>
2015-11-02 15:22:02 +0000

    we need to wait a lot longer for the network to come up; and use a terminal for the test run

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ nova secgroup-add-rule gen_sg icmp 0 255 0.0.0.0/0
 nova secgroup-add-rule gen_sg tcp 22 22 0.0.0.0/0
 nova keypair-add --pub-key ~/.ssh/id_rsa.pub gen_kp
 nova boot --image="CentOS7" --flavor="m1.small" --key_name gen_kp --security-groups gen_sg i1
-sleep 5
+sleep 30
 
 # get the netns setup and wait for ping to start working
 net_id=$(neutron router-list -F id -f value )
@@ -33,5 +33,5 @@ ssh_cmd="ip netns exec qrouter-${net_id} ssh centos@${IP}"
 # we just do this to make sure the ssh works
 $ssh_cmd uname -a
 ip netns exec qrouter-${net_id} scp test_script.sh centos@${IP}:
-$ssh_cmd bash test_script.sh
+$ssh_cmd -t bash test_script.sh
 exit $?
