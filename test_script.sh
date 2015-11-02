ca1bf5fc623f8da00846b32a176f56fe4c5f94c1
Karanbir Singh <kbsingh@karan.org>
2015-11-02 15:27:22 +0000

    force external dns

--- test_script.sh
+++ test_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo nameserver 8.8.8.8 > resolv.conf
+sudo cp -f resolv.conf /etc/resolv.conf
+
 sudo yum -y install git 
 git clone https://git.centos.org/git/sig-core/t_functional
 cd t_functional
